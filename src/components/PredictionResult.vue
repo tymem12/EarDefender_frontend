b897427c0d28e49bd7abc522d517295d5b2f6df0
Lukasz Janiak <janiaklukasz11@gmail.com>
2024-12-06 06:16:05 +0100

    Update Home Page

--- src/components/PredictionResult.vue
+++ src/components/PredictionResult.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="bg-white shadow-lg p-6 rounded-lg mb-10" style="width: 90%; margin-left: auto; margin-right: auto; margin-bottom: 20px;">
+  <div
+    class="bg-white shadow-lg p-6 rounded-lg mb-10"
+    style="width: 90%; margin-left: auto; margin-right: auto; margin-bottom: 20px;"
+  >
     <!-- Header -->
     <div class="flex justify-between items-start border-b pb-4 mb-4">
       <!-- Left Column: Details -->
